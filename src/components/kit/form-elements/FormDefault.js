cc7aac5a768a8ce185f0fd1a322676188bdc41a1
Prabesh Shakya <prabeshshakya@Prabeshs-MacBook-Pro-3.local>
2019-10-16 12:43:27 +0545

    fix few design issues

--- src/components/kit/form-elements/FormDefault.js
+++ src/components/kit/form-elements/FormDefault.js
@@ -4,26 +4,43 @@ import { Syntax } from '../../common/syntax-highlighter';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const FormDefault = () => {
-const code=`<div>
-    <button class="btn btn--primary mr--3x">Button</button>
-    <button class="btn btn--primary" disabled>Button</button>
-</div>`
+const code = `<div class="row">
+    <div class="col--7--md">
+        <form action="">
+            <div class="form-group">
+                <label class="form-group__label">Email Address</label>
+                <input type="email" class="form-group__control" placeholder="Email Address"/>
+            </div>
+            <div class="form-group">
+                <label class="form-group__label">Password</label>
+                <input type="password" class="form-group__control" placeholder="Your Secret"/>
+            </div>
+            <div class="form-group c-checkbox">
+                <input type="checkbox" class="c-checkbox__control" id="checkbox"/>
+                <label htmlFor="checkbox">Check Me</label>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn--primary btn--rounded">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>`;
+
+const SampleUsageCode = `<div className="form-group">
+    <label className="form-group__label">Email Address</label>
+    <input type="email" className="form-group__control" placeholder="Email Address"/>
+</div>`;
 
     return(
         <div className="doc-container">
                 <h4 className="title title--medium title--dark mb--2x">Basic Usage</h4>
                 <p>
-                    In-order to use this component, add <code>.btn</code> class and add modifiers such as <code>.btn--primary</code> to <code>button</code> tag.
+                    Below is the sample code that generates a email input field.
                 </p>
                 <div className="code-preview">
                     <div className="mb--2x">
                         <Syntax>
-                            {'<button class="btn btn--primary"></button>'}
-                        </Syntax>
-                    </div>
-                    <div>
-                        <Syntax>
-                            {'<button class="btn btn--primary" disabled></button>'}
+                            {SampleUsageCode}
                         </Syntax>
                     </div>
                 </div>
@@ -36,25 +53,27 @@ const code=`<div>
 
                         <TabPanel>
                             <div className="preview-container">
-                                <form action="">
-                                    <div className="form-group">
-                                        <label className="form-group__label">Email address</label>
-                                        <input type="email" className="form-group__control" placeholder="Email Address"/>
+                                <div className="row">
+                                    <div className="col--7--md">
+                                        <form action="">
+                                            <div className="form-group">
+                                                <label className="form-group__label">Email Address</label>
+                                                <input type="email" className="form-group__control" placeholder="Email Address"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <label className="form-group__label">Password</label>
+                                                <input type="password" className="form-group__control" placeholder="Your Secret"/>
+                                            </div>
+                                            <div className="form-group c-checkbox">
+                                                <input type="checkbox" className="c-checkbox__control" id="checkbox"/>
+                                                <label htmlFor="checkbox">Check Me</label>
+                                            </div>
+                                            <div className="form-group">
+                                                <button type="submit" className="btn btn--primary btn--rounded">Submit</button>
+                                            </div>
+                                        </form>
                                     </div>
-                                    <div className="form-group">
-                                        <label className="form-group__label">Password</label>
-                                        <input type="password" className="form-group__control" placeholder="Your Secret"/>
-                                    </div>
-                                    <div className="form-group c-checkbox">
-                                        <input type="checkbox" className="c-checkbox__control" id="checkbox"/>
-                                        <label htmlFor="checkbox">Check Me</label>
-                                    </div>
-                                    <div className="form-group">
-                                        <button type="submit" className="btn btn--primary btn--rounded">Submit</button>
-                                    </div>
-
-                                </form>
-                               
+                                </div>
                             </div>
                         </TabPanel>
                         <TabPanel>
