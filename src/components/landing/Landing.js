5a91fc8c8f9cbab400482fb32666eb8e9d676895
Prabesh Shakya <prabeshshakya@Prabeshs-MacBook-Pro-3.local>
2019-10-16 15:56:26 +0545

    fix spacing variable dynamic and fixed font issues

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return(
-        <div className="kit-landing-page d-flex justify-content-center align-items-center">
+        <div className="kit-landing-page d-flex justify-content--center align-items--center">
             <div className="kit-landing-page__logo">
                 <h1 className="font-weight--thin">Project Starter Kit</h1>
-                <div className="action--button text--center">
+                <div className="action-button text--center">
                     <Link to="/docs" className="btn btn--primary btn--large btn--rounded">Docs</Link>
                 </div>
             </div>
