b48193154729de1ec77553554d44de7b5fa45dca
elizaRjb <elizarajbhandari@lftechnology.com>
2019-10-22 14:53:10 +0545

    Add basic Alert component

--- src/components/kit/alert/Alert.js
+++ src/components/kit/alert/Alert.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-const Alert = () => <span>alert here</span>
+import AlertDefault from './AlertDefault';
+
+const Alert = () => {
+  return (
+    <div className="mt--11x">
+      <div className="title-area  mb--7x">
+        <h2 className="title title--largest title--dark font-weight--thin mb--4x">Alerts</h2>
+        <p className="lead-text">Create alert messages with variety of Styles</p>
+      </div>
+
+      <div className="mb--11x">
+        <AlertDefault />
+      </div>
+
+      {/* <div className="mb--11x">
+        <ButtonVariants />
+      </div>
+
+      <div className="mb--11x">
+        <ButtonSize />
+      </div>
+
+      <div className="mb--11x">
+        <ButtonShadow />
+      </div> */}
+    </div>
+  );
+}
 
 export default Alert;
