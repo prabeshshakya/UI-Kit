de85f1a0227a62a0eb92ad22fd07261975d74add
ajaydangol014 <ajaydangol014@gmail.com>
2022-05-02 15:11:42 +0545

    Fix form elements page design

--- src/components/kit/form-elements/FormElements.js
+++ src/components/kit/form-elements/FormElements.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import FormDefault from './FormDefault';
-import ButtonVariants from './ButtonVariants';
-import ButtonSize from './ButtonSize';
-import ButtonShadow from './ButtonShadow';
+import FormDefault from "./FormDefault";
+import ButtonVariants from "../button/ButtonVariants";
+import ButtonSize from "../button/ButtonSize";
+import ButtonShadow from "../button/ButtonShadow";
 
 const FormElements = () => {
-    return(
-        <div className="mt--11x">
-            <div className="title-area  mb--7x">
-                <h2 className="title title--largest title--dark font-weight--thin mb--4x">Form Elements</h2>
-                <p className="lead-text">Examples for creating form elements</p>
-            </div>
+  return (
+    <div className="page mt--11x">
+      <div className="title-area  mb--7x">
+        <h2 className="title title--largest title--dark font-weight--thin mb--4x">
+          Form Elements
+        </h2>
+        <p className="lead-text">Examples for creating form elements</p>
+      </div>
 
-            <div className="mb--11x">
-                <FormDefault />
-            </div>
+      <div className="mb--11x">
+        <FormDefault />
+      </div>
 
-            <div className="mb--11x">
-                <ButtonVariants />
-            </div>
+      <div className="mb--11x">
+        <ButtonVariants />
+      </div>
 
-            <div className="mb--11x">
-                <ButtonSize />
-            </div>
+      <div className="mb--11x">
+        <ButtonSize />
+      </div>
 
-            <div className="mb--11x">
-                <ButtonShadow />
-            </div>
-        </div>
-    );
-}
+      <div className="mb--11x">
+        <ButtonShadow />
+      </div>
+    </div>
+  );
+};
 
 export default FormElements;
