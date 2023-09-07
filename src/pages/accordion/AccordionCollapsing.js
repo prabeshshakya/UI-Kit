82f549556b58aa7f3a99c07027e665d39e430c7b
Roman-Awale <128462242+Roman-Awale@users.noreply.github.com>
2023-09-07 14:05:00 +0545

    Doc enhancement (#73)
    * doc-enhancement: documentation
    
    * doc enhancement in introduction page and fix flex align content
    
    * added spacing in paragraph tag
    
    * document enhancement in setup page
    
    * added download link in sidebar
    
    * change in folder structure
    
    ---------
    
    Co-authored-by: Roman-Awale <romanawale@gmail.com>

--- src/pages/accordion/AccordionCollapsing.js
+++ src/pages/accordion/AccordionCollapsing.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { Syntax } from '../../docs/syntax-highlighter';
-import Accordion from './Accordion';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { Syntax } from "../../docs/syntax-highlighter";
+import Accordion from "./Accordion";
 
 const AccordionCollapsing = ({ accordionData }) => {
-	const code = ` <div className="accordion">
+  const code = ` <div className="accordion">
     <div className="accordion__head">
         <a href="javascript:void(0)" className="accordion__title">Accordion Item Title</a>
         <span className="accordion__icon">+</span>
@@ -40,34 +40,31 @@ const AccordionCollapsing = ({ accordionData }) => {
     </p>
   </div>`;
 
-	return (
-		<div className='usage-markup-preview'>
-			<Tabs>
-				<TabList>
-					<Tab>Preview</Tab>
-					<Tab>Code</Tab>
-				</TabList>
-				<TabPanel>
-					<div className='preview-container'>
-						{accordionData.map(({ title, content }) => (
-							<Accordion
-								title={title}
-								content={content}
-							/>
-						))}
-					</div>
-				</TabPanel>
-				<TabPanel>
-					<div className='preview-container'>
-						<div className='d-flex'>
-							<div className='code-preview'>
-								<Syntax>{code}</Syntax>
-							</div>
-						</div>
-					</div>
-				</TabPanel>
-			</Tabs>
-		</div>
-	);
+  return (
+    <div className="usage-markup-preview">
+      <Tabs>
+        <TabList>
+          <Tab>Preview</Tab>
+          <Tab>Code</Tab>
+        </TabList>
+        <TabPanel>
+          <div className="preview-container">
+            {accordionData.map(({ title, content }) => (
+              <Accordion title={title} content={content} />
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="preview-container">
+            <div className="d-flex">
+              <div className="code-preview">
+                <Syntax>{code}</Syntax>
+              </div>
+            </div>
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 export default AccordionCollapsing;
---
+++ src/pages/introduction/intro/Folderstructure.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Syntax } from "../../../docs/syntax-highlighter";
+
+const Folderstructure = () => {
+  return (
+    <div className="page">
+      <div class="content-block">
+        <h3 id="folderstructure">Folder Structure</h3>
+
+        <div class="folder-structure">
+          <ul className="tree">
+            <li>src/</li>
+            <li className="has-subtree">
+              <ul className="tree">
+                <li>assets/</li>
+                <li className="has-subtree">
+                  <ul className="tree">
+                    <li>images/</li>
+                    <li>scss/</li>
+
+                    <li className="has-subtree">
+                      <ul className="tree">
+                        <li>base/</li>
+
+                        <li className="has-subtree">
+                          <ul className="tree">
+                            <li>__index.scss</li>
+                            <li>_base.scss</li>
+                            <li>_folder.scss</li>
+                            <li>_normalize.scss</li>
+                            <li>_typography.scss</li>
+                          </ul>
+                        </li>
+                        <li>components/</li>
+                        <li className="has-subtree">
+                          <ul className="tree">
+                            <li>__index.scss</li>
+                          </ul>
+                        </li>
+                        <li>layouts/</li>
+                        <li className="has-subtree">
+                          <ul className="tree">
+                            <li>__index.scss</li>
+                            <li>_footer.scss</li>
+                            <li>_header.scss</li>
+                            <li>_sidebar.scss</li>
+                          </ul>
+                        </li>
+                        <li>mixins/</li>
+                        <li className="has-subtree">
+                          <ul className="tree">
+                            <li>__index.scss</li>
+                            <li>_bem.scss</li>
+                            <li>_font-import-util.scss</li>
+                            <li>_general.scss</li>
+                            <li>_responsive.scss</li>
+                            <li>_typography.scss</li>
+                          </ul>
+                        </li>
+                        <li>utilities/</li>
+                        <li className="has-subtree">
+                          <ul className="tree">
+                            <li>classes/</li>
+                            <li className="has-subtree">
+                              <ul className="tree">
+                                <li>__index.scss</li>
+                                <li>_bem.scss</li>
+                                <li>_font-import-util.scss</li>
+                                <li>_general.scss</li>
+                                <li>_responsive.scss</li>
+                                <li>_typography.scss</li>
+                              </ul>
+                            </li>
+                            <li>functions/</li>
+                            <li className="has-subtree">
+                              <ul className="tree">
+                                <li>utils/</li>
+                                <li className="has-subtree">
+                                  <ul className="tree">
+                                    <li>__index.scss</li>
+                                    <li>_deep-map-check.scss</li>
+                                    <li>_deep-map-get.scss</li>
+                                    <li>_sdeep-map-merge.scss</li>
+                                    <li>_get.scss</li>
+                                    <li>_str-explode.scss</li>
+                                  </ul>
+                                </li>
+                                <li>__index.scss</li>
+                                <li>_helper/scss</li>
+                                <li>_unit-utils</li>
+                              </ul>
+                            </li>
+                            <li>__index.scss</li>
+                          </ul>
+                        </li>
+                        <li>variables/</li>
+                        <li className="has-subtree">
+                          <ul className="tree">
+                            <li>__index.scss</li>
+                            <li>_color.scss</li>
+                            <li>_config.scss</li>
+                            <li>_easing.scss</li>
+                            <li>_grid.scss</li>
+                            <li>_spacing.scss</li>
+                            <li>_typography.scss</li>
+                            <li>_utilities.scss</li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Folderstructure;
