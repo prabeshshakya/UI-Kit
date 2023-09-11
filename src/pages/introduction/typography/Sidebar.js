02768417e272d68c62f0d90aca10ad0468294f46
Roman-Awale <128462242+Roman-Awale@users.noreply.github.com>
2023-09-11 10:35:33 +0545

    added variable in typography. (#74)
    * added css variable in typography
    
    * combined responsive doc into breakpoint
    
    ---------
    
    Co-authored-by: Roman-Awale <romanawale@gmail.com>

--- src/pages/introduction/typography/Sidebar.js
+++ src/pages/introduction/typography/Sidebar.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import { NavHashLink } from 'react-router-hash-link';
+import React from "react";
+import { NavHashLink } from "react-router-hash-link";
 
 const Sidebar = () => {
-	const scrollWithOffset = (el) => {
-		const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-		const yOffset = -100;
-		window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
-	};
+  const scrollWithOffset = (el) => {
+    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+    const yOffset = -100;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
+  };
 
-	return (
-		<div className='sidebar--right'>
-			<div className='menu-group'>
-				<div className='menu-group__title'>on this page</div>
-				<NavHashLink
-					smooth
-					to='/typography/#global-setting'
-					className='menu-group__nodes__single'
-					activeClassName='menu-group__nodes__single--active'
-					scroll={(el) => scrollWithOffset(el)}
-				>
-					Global setting
-				</NavHashLink>
-				<NavHashLink
-					smooth
-					to='/typography/#heading'
-					className='menu-group__nodes__single'
-					activeClassName='menu-group__nodes__single--active'
-					scroll={(el) => scrollWithOffset(el)}
-				>
-					Heading
-				</NavHashLink>
-			</div>
-		</div>
-	);
+  return (
+    <div className="sidebar--right">
+      <div className="menu-group">
+        <div className="menu-group__title">on this page</div>
+        <NavHashLink
+          smooth
+          to="/typography/#global-setting"
+          className="menu-group__nodes__single"
+          activeClassName="menu-group__nodes__single--active"
+          scroll={(el) => scrollWithOffset(el)}
+        >
+          Global setting
+        </NavHashLink>
+        <NavHashLink
+          smooth
+          to="/typography/#heading"
+          className="menu-group__nodes__single"
+          activeClassName="menu-group__nodes__single--active"
+          scroll={(el) => scrollWithOffset(el)}
+        >
+          Heading
+        </NavHashLink>
+        <NavHashLink
+          smooth
+          to="/typography/#variables"
+          className="menu-group__nodes__single"
+          activeClassName="menu-group__nodes__single--active"
+          scroll={(el) => scrollWithOffset(el)}
+        >
+          Variables
+        </NavHashLink>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
