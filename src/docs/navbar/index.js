99f05111d61dd6bf6c04b8f2dc6f88c051c26894
Pratik Sangami <9841pratik@gmail.com>
2024-02-01 14:02:30 +0545

    Fix image usage as react component

--- src/docs/navbar/index.js
+++ src/docs/navbar/index.js
@@ -1,29 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { logo } from '../../assets/images';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Logo } from "../../assets/images";
 
 const Nav = () => {
-	return (
-		<nav className='navbar'>
-			<div className='container navbar__flex'>
-				<div className='navbar__header'>
-					<img
-						src={logo}
-						alt='startkit Logo'
-						width={120}
-					/>
-				</div>
-				<div className='navbar__list'>
-					<NavLink
-						to='/'
-						className='navbar__item'
-					>
-						Github
-					</NavLink>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="container navbar__flex">
+        <div className="navbar__header">
+          <Logo />
+        </div>
+        <div className="navbar__list">
+          <NavLink to="/" className="navbar__item">
+            Github
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
